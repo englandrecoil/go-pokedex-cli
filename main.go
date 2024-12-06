b291c2f13c03e44ff63d273e3b57e0e62628522d
englandrecoil <nik.tereshchenko.982@gmail.com>
2024-12-06 23:43:22 +0300

    refactor: minor changes for code readability

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/englandrecoil/go-pokedex-cli/internal/pokeapi"
@@ -42,6 +43,11 @@ var commands = map[string]command{
 		description: "Displays the names of the previous 20 location areas",
 		callback:    commandBackMap,
 	},
+	"explore": {
+		name:        "explore {location}",
+		description: "List all the Pokémon in a given area",
+		callback:    commandExplore,
+	},
 	"cache": {
 		name:        "cache",
 		description: "Set the caching interval after which cleaning will occur",
@@ -52,33 +58,37 @@ var commands = map[string]command{
 type command struct {
 	name        string
 	description string
-	callback    func(*pokeapi.Config) error
+	callback    func(*pokeapi.Config, ...string) error
 }
 
 func printPrompt() {
 	fmt.Print(cliName, "> ")
 }
 
-func commandHelp(cfg *pokeapi.Config) error {
+func commandHelp(cfg *pokeapi.Config, param ...string) error {
+	if param == nil {
+		fmt.Println("PARAM IS NIL")
+	}
 	fmt.Println("Usage:")
-	fmt.Println("  help\t\tDisplays a help message")
-	fmt.Println("  exit\t\tExit the Pokedex")
-	fmt.Println("  clear\t\tClear the terminal screen")
-	fmt.Println("  map\t\tDisplays the names of the next 20 location areas")
-	fmt.Println("  bmap\t\tDisplays the names of the previous 20 location areas")
-	fmt.Println("  cache\t\tSet the caching interval after which cleaning will occur")
-	fmt.Println("  \t\t(default value is 10 seconds)")
+	fmt.Println("  help\t\t\t\tDisplays a help message")
+	fmt.Println("  exit\t\t\t\tExit the Pokedex")
+	fmt.Println("  clear\t\t\t\tClear the terminal screen")
+	fmt.Println("  map\t\t\t\tDisplays the names of the next 20 location areas")
+	fmt.Println("  bmap\t\t\t\tDisplays the names of the previous 20 location areas")
+	fmt.Println("  explore {location_area}\tDisplays all the Pokémon in a given area")
+	fmt.Println("  cache\t\t\t\tSet the caching interval after which cleaning will occur")
+	fmt.Println("  \t\t\t\t(default value is 10 seconds)")
 	fmt.Println("")
 
 	return nil
 }
 
-func commandExit(cfg *pokeapi.Config) error {
+func commandExit(cfg *pokeapi.Config, params ...string) error {
 	defer os.Exit(0)
 	return nil
 }
 
-func commandClear(cfg *pokeapi.Config) error {
+func commandClear(cfg *pokeapi.Config, params ...string) error {
 	cmd := exec.Command("clear")
 	cmd.Stdout = os.Stdout
 	if err := cmd.Run(); err != nil {
@@ -87,7 +97,7 @@ func commandClear(cfg *pokeapi.Config) error {
 	return nil
 }
 
-func commandMap(cfg *pokeapi.Config) error {
+func commandMap(cfg *pokeapi.Config, params ...string) error {
 	locationAreas, err := pokeapi.GetLocationAreas(cfg, pokeapi.Next)
 	if err != nil {
 		return fmt.Errorf("map command error: %s", err)
@@ -100,7 +110,7 @@ func commandMap(cfg *pokeapi.Config) error {
 	return nil
 }
 
-func commandBackMap(cfg *pokeapi.Config) error {
+func commandBackMap(cfg *pokeapi.Config, params ...string) error {
 	locationAreas, err := pokeapi.GetLocationAreas(cfg, pokeapi.Previous)
 	if err != nil {
 		return fmt.Errorf("map command error: %s", err)
@@ -113,7 +123,7 @@ func commandBackMap(cfg *pokeapi.Config) error {
 	return nil
 }
 
-func commandCache(cfg *pokeapi.Config) error {
+func commandCache(cfg *pokeapi.Config, params ...string) error {
 	fmt.Println("Enter the caching interval")
 	reader := bufio.NewScanner(os.Stdin)
 	reader.Scan()
@@ -128,9 +138,19 @@ func commandCache(cfg *pokeapi.Config) error {
 	return nil
 }
 
+func commandExplore(cfg *pokeapi.Config, params ...string) error {
+	if len(params) == 1 {
+		return fmt.Errorf("explore command error: no location provided")
+	}
+
+	return nil
+}
+
 func defineCommand(input string, cfg *pokeapi.Config) error {
-	if input, exists := commands[input]; exists {
-		if err := input.callback(cfg); err != nil {
+	cleanedInput := strings.Fields(input)
+
+	if command, exists := commands[cleanedInput[0]]; exists {
+		if err := command.callback(cfg, cleanedInput...); err != nil {
 			return err
 		}
 		return nil
@@ -139,18 +159,23 @@ func defineCommand(input string, cfg *pokeapi.Config) error {
 	return err
 }
 
-func main() {
-	cfg := &pokeapi.Config{}
-	cfg.Cache = pokecache.NewCache(time.Duration(interval))
-	reader := bufio.NewScanner(os.Stdin)
+func printWelcomeMessage() {
 	fmt.Printf("Welcome to the Pokedex!\n\n")
 	fmt.Println("Please note that the Pokedex CLI is using a cache to quickly")
 	fmt.Println("access data and reduce the load on the PokeAPI servers")
 	fmt.Printf("\n")
 	fmt.Println("Use 'help' command to find out about Pokémon world exploration commands.")
 	fmt.Println()
+}
 
+func main() {
+	cfg := &pokeapi.Config{}
+	cfg.Cache = pokecache.NewCache(time.Duration(interval))
+	reader := bufio.NewScanner(os.Stdin)
+
+	printWelcomeMessage()
 	printPrompt()
+
 	for reader.Scan() {
 		if err := defineCommand(reader.Text(), cfg); err != nil {
 			if errors.Is(err, errUndefinedCommand) {
